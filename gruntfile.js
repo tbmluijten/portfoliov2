c70b623f27c09b4973bb67048c89abcb4d9c6254
updated <TomLuijten@MacBook-Pro-van-Tom-2.local>
2014-06-16 09:16:49 +0200

    Grunt server fix

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,55 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     sass: {
       dist: {
+                options: {
+                  noCache: true
+                },
         files: {
-          'css/main.css' : 'sass/main.sass'
+          'css/main.css': 'sass/main.sass'
         }
       }
     },
+
     watch: {
+            options: {
+                livereload: true
+            },
       css: {
         files: 'sass/**/*.sass',
-        tasks: ['sass']
+                tasks: ['sass', 'autoprefixer']
+
       },
-      html: {
-        files: '*.html'
-      }
-    }
+            html: {
+                files: '*.html'
+
+            }
+    },
+
+        connect: {
+            options: {
+                port: 9001,
+                livereload: 35729,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '.'
+                    ]
+                }
+            }
+        },
+
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('default',['sass', 'watch']);
+    grunt.registerTask('serve', ['connect', 'default', 'watch']);
 }
